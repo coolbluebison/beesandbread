c056e7a57a2cb17230643c5e9e82606aaaa2a391
Tri <trihoang09@gmail.com>
2024-01-06 21:28:33 -0700

    more nav work

--- client/src/components/Layout/Footer Components/FooterBanner.jsx
+++ client/src/components/Layout/Footer Components/FooterBanner.jsx
@@ -2,7 +2,7 @@
 
 export default function FooterBanner(){
     return(
-        <div className="w-full bg-rose-200 h-16 flex items-center justify-evenly text-black">
+        <div className="w-full bg-rose-200 h-16 flex items-center justify-between px-52 text-black">
             <p className=" font-bold text-lg">Need Help?</p>
             <div className="flex items-center justify-end gap-3 w-1/2">
                 <p className="w-1/3 h-10 rounded-3xl bg-white flex items-center justify-center text-center cursor-pointer">Email Us</p>
--- client/src/components/Layout/NavBarComponents/Drawer.jsx
+++ client/src/components/Layout/NavBarComponents/Drawer.jsx
@@ -1,9 +1,17 @@
 import SubDrawer from "./SubDrawer"
+import DrawerMain from "./DrawerComponents/DrawerMain"
+import CategoryDrawer from "./DrawerComponents/CategoryDrawer"
+
+
 import { useState } from "react"
 
-export default function DrawerMain(){
+
+export default function Drawer(){
 
     let [sub,setSub] = useState(false)
+    let [display,setDisplay] = useState(true)
+    let [subContent,setSubContent] = useState(null)
+
 
     return(
         <div className="drawer-side z-50">
@@ -13,38 +21,9 @@ export default function DrawerMain(){
                             <div className="w-80">
                                 <p className="text-center font-semibold text-black p-4 text-lg w full bg-rose-300">Welcome Back 'User'!</p>
                                 <p className="w-full h-[0.5px] mb-2 bg-gray-400"></p>
-                                    <p className="flex items-center text-2xl text-gray-900 font-bold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1" onClick={()=>setSub(!sub)}>Categories
-                                    <svg className="ml-auto h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M10 7L15 12L10 17" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg></p> 
-                                    <p className="flex items-center text-2xl text-gray-900 font-bold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1">Brand
-                                    <svg className="ml-auto h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M10 7L15 12L10 17" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg></p>
-                                    <p className="flex items-center text-2xl text-gray-900 font-bold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1">Diet
-                                    <svg className="ml-auto h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M10 7L15 12L10 17" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg></p>
-                                    <p className="flex items-center text-2xl text-gray-900 font-bold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1">But It Again
-                                    <svg className="ml-auto h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M10 7L15 12L10 17" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg></p>
-                                    <p className="flex items-center text-2xl text-gray-900 font-bold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1">Deals
-                                    <svg className="ml-auto h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M10 7L15 12L10 17" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg></p>
-                                <p className="w-full h-[0.5px] my-2 bg-gray-400"></p>
-                                    <p className="text-lg text-gray-700 font-semibold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1">My Orders</p>
-                                    <p className="text-lg text-gray-700 font-semibold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1">Autoship</p>
-                                    <p className="text-lg text-gray-700 font-semibold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1">My Account</p>
-                                    <p className="text-lg text-gray-700 font-semibold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1">Shopping List</p>
-                                    <p className="text-lg text-gray-700 font-semibold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1">Blog</p>
-                                <p className="w-full h-[0.5px] my-2 bg-gray-400"></p>
-                                    <p className="text-lg text-gray-700 font-semibold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1">About Us</p>
-                                    <p className="text-lg text-gray-700 font-semibold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1">How It Works</p>
-                                    <p className="text-lg text-gray-700 font-semibold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1">Our Quality Standard</p>
-                                    <p className="text-lg text-gray-700 font-semibold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1">Enviromental Impact</p>
-                                    <p className="text-lg text-gray-700 font-semibold w-full hover:bg-gray-400 transition-all ease-linear cursor-pointer duration-75 px-4 py-1">FAQ</p>
+                                <div className="transition-all duration-500">
+                                {display ? <DrawerMain setDisplay={setDisplay} /> : <CategoryDrawer setDisplay={setDisplay} setSub={setSub}/>}
+                                </div>
                             </div>
                             <SubDrawer sub={sub} setSub={setSub}/>
                         </div>
